c3c3a4544e968034f015628b5732231d4f19be4a
pinganzhang <pinganzhang@tencent.com>
2020-07-01 02:04:22 +0800

    add

--- src/test/Main.java
+++ src/test/Main.java
@@ -6,6 +6,8 @@ import Common.Sword;
 import model.ListNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
@@ -61,5 +63,20 @@ public class Main {
         commonArray.printArray(arr);
         commonArray.mergeSort(arr);
         commonArray.printArray(arr);
+
+        /*
+            字节面试题
+         */
+        List<Long> longArrayList = new ArrayList();
+        List<Integer> integerArrayList = new ArrayList();
+        List<Boolean> booleanLinkedList = new LinkedList();
+        longArrayList.add(10L);
+        longArrayList.add(20L);
+        integerArrayList.add(10);
+        integerArrayList.add(20);
+        booleanLinkedList.add(true);
+        booleanLinkedList.add(false);
+        System.out.println(longArrayList.getClass() == integerArrayList.getClass());
+        System.out.println(longArrayList.getClass() == booleanLinkedList.getClass());
     }
 }
