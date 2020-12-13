97e4734b871c8af2f901813211a9eb14cd1da174
pinganzhang <1596454155@qq.com>
2020-12-13 23:29:33 +0800

    JavaStudy

---
+++ src/Study/ArrayAndList.java
@@ -0,0 +1,12 @@
+package Study;
+
+import java.util.ArrayList;
+
+public class ArrayAndList {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("hello");
+        arrayList.add("world");
+        System.out.println(arrayList);
+    }
+}
---
+++ src/Study/StringAndStringBuilder.java
@@ -0,0 +1,24 @@
+package Study;
+
+public class StringAndStringBuilder {
+    public static void main(String[] args) {
+        /**
+         * StringBuilder 和 String
+         */
+        String s = "abc";
+        StringBuilder stringBuilder = new StringBuilder(s);
+
+        StringBuilder stringBuilder1 = new StringBuilder("hello");
+
+        String s1 = stringBuilder1.toString();
+
+        System.out.println(s1);
+
+        //  拼接
+        StringBuilder stringBuilder2 = new StringBuilder();
+        stringBuilder2.append("hello,").append("Java");
+        System.out.println(stringBuilder2.toString());
+        //  反转
+        System.out.println(stringBuilder2.reverse().toString());
+    }
+}
